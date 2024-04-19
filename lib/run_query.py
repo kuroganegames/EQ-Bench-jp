88e7d165810f5e5d3dc6b10cd6b732a49f126979
sqrkl <152407511+sqrkl@users.noreply.github.com>
2024-04-19 22:49:13 +1000

    fix

--- lib/run_query.py
+++ lib/run_query.py
@@ -25,9 +25,6 @@ def run_chat_query(prompt, completion_tokens, model, tokenizer, temp):
 	return response
 
 def run_pipeline_query(prompt, completion_tokens, model, tokenizer, temp):
-	if type(prompt) == str:
-		toks = tokenizer(prompt)
-	n_toks = len(toks['input_ids'])
 	text_gen = pipeline(task="text-generation", model=model, tokenizer=tokenizer, do_sample=True, temperature=temp, max_new_tokens=completion_tokens)
 	output = text_gen(prompt)
 	out_str = output[0]['generated_text']
